60feaf7d8d8bf697b9952330556734223a1e1006
hamza haj salem <hajsalemhamza@gmail.com>
2020-12-15 09:18:54 +0100

    TP1_UDP_Messagerie_Client_Server

---
+++ src/Instant_Messaging_ENIG/SendServer.java
@@ -0,0 +1,12 @@
+package Instant_Messaging_ENIG;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramSocket;
+
+public class SendServer extends Thread {
+	
+	
+
+}
